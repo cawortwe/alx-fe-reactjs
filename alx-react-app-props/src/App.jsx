5f8ab789d293ae27d87f38918f57f82826356ff2
Christopher Awortwe <cawortwe35@gmail.com>
2025-11-13 23:19:31 -0800

    initial commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,8 +3,11 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
     <>
       <WelcomeMessage />
@@ -12,6 +15,9 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
     </>
   );
 }
