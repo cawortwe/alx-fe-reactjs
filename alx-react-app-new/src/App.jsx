538b94c3a72d548d40e9b9cbd6ffd88b1903b9ac
Christopher Awortwe <cawortwe35@gmail.com>
2025-11-13 22:38:24 -0800

    initial commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
     </>
   );
 }
