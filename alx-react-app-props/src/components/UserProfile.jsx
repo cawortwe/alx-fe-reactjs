9d683d37690f69a7ec67a89a3e43be9121731a19
Christopher Awortwe <cawortwe35@gmail.com>
2025-11-13 23:13:30 -0800

    initial commit

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+export default function UserProfile() {
+  return (
+    <div
+      style={{
+        border: "2px solid blue",
+        padding: "10px",
+        margin: "10px",
+        color: "darkblue",
+      }}
+    >
+      <span style={{ fontWeight: "bold" }}>Username:</span> Katey Kwesi
+    </div>
+  );
+}
