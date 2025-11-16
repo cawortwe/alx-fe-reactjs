693ed1400d93a176979e6679d6da6b29f46ce8b6
Christopher Awortwe <cawortwe35@gmail.com>
2025-11-16 22:01:43 +0000

    initial commit

---
+++ recipe-sharing-app/src/stores/useRecipeStore.js
@@ -0,0 +1,35 @@
+import create from 'zustand';
+
+const useRecipeStore = create((set, get) => ({
+  recipes: [],
+  favorites: [],
+  recommendations: [],
+
+ 
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+
+  setRecipes: (recipes) => set({ recipes }),
+
+
+  addFavorite: (recipeId) =>
+    set((state) => ({ favorites: [...state.favorites, recipeId] })),
+
+ 
+  removeFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: state.favorites.filter((id) => id !== recipeId),
+    })),
+
+
+  generateRecommendations: () => {
+    const state = get();
+    const recommended = state.recipes.filter(
+      (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
+    );
+    set({ recommendations: recommended });
+  },
+}));
+
+export default useRecipeStore;
