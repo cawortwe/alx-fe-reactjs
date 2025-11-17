eba59806a500332dc09baaa63331438a140515c4
Christopher Awortwe <cawortwe35@gmail.com>
2025-11-16 18:14:09 -0800

    initial commit

---
+++ my-company/src/components/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Us</h1>
+      <p style={{ lineHeight: 1.7 }}>
+        Our company has been providing top-notch services since 1990. We
+        specialize in various fields including technology, marketing, and
+        consultancy. Our mission is to help our clients grow with modern
+        solutions, clear strategy, and exceptional delivery.
+      </p>
+    </div>
+  );
+}
+
+export default About;
