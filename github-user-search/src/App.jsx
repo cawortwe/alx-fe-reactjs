0b22b9c12ecae8f13fe4e774fc185e79f2a0bd0b
Christopher Awortwe <cawortwe35@gmail.com>
2025-11-23 21:47:35 +0000

    initial commit

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Search from "./components/Search";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Layout({ children }) {
+  const key = import.meta.env.VITE_APP_GITHUB_API_KEY;
+  console.log(key);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      <header style={styles.header}>
+        <nav></nav>
+      </header>
+
+      <main style={styles.main}>{children}</main>
+      <div style={{ padding: "2rem" }}>
+        <Search />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function Home() {
+  return;
+}
+
+function About() {
+  return <h2>About This Application</h2>;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+const styles = {
+  header: {
+    padding: "1rem",
+    background: "#222",
+    color: "#fff",
+    display: "flex",
+    gap: "1rem",
+  },
+  link: {
+    color: "#fff",
+    textDecoration: "none",
+    marginRight: "1rem",
+  },
+  main: {
+    padding: "2.1rem",
+  },
+  footer: {
+    padding: "1rem",
+    background: "#f4f4f4",
+    marginTop: "2rem",
+    textAlign: "center",
+  },
+};
